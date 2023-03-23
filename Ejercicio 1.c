11493453cc1d7a2beb50a489064cca474c14fda5
alemonzon <125765378+alemonzon@users.noreply.github.com>
2023-03-23 15:49:16 -0600

    Create Ejercicio 1.c

---
+++ Ejercicio 1.c
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------------
+// Alejandro Monzón (21560)
+// CORTO 3 - EJERCICIO 1
+// Mostrar nombre y carné
+//------------------------------------------------------------------------------
+
+#include <stdio.h>
+
+int main() {
+    char nombre[] = "Alejandro Monzón"; //nombre
+    int carne = 21560; //carne
+    
+    //Mostrar valores
+    printf("Mi nombre es %s y mi carné es %d\n", nombre, carne);
+    
+    return 0;
+}
